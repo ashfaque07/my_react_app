45b8b49188d54d5046776f28a96b670caa2552d1
Ashfaque <ashfaque.b.shaikh@accenture.com>
2020-03-05 00:52:58 +0530

    View & Components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import textView from './views/textView';
+import ClickCounter from './components/ClickCounter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          fgdg
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <ClickCounter/>
       </header>
     </div>
   );
---
+++ src/components/ClickCounter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ClickCounter extends React.Component {
+
+	render(){
+		return (
+			<div>
+				<button>Click Me</button>
+				<p>Counter</p>
+			</div>
+		)
+	}
+}
+
+export default ClickCounter;
