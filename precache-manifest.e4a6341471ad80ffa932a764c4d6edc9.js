76ebce6fb1372f489b50dd7b0a6ad326d8614817
Ashfaque <ashfaque.b.shaikh@accenture.com>
2020-03-04 22:28:41 +0530

    Updates

---
+++ precache-manifest.e4a6341471ad80ffa932a764c4d6edc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19b43791f84dd9dac9aad121958ce6b",
+    "url": "/my_react_app/index.html"
+  },
+  {
+    "revision": "7bea19b0c7c44611b4a2",
+    "url": "/my_react_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bac87a6a7e53371dea1e",
+    "url": "/my_react_app/static/js/2.aead8d47.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my_react_app/static/js/2.aead8d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bea19b0c7c44611b4a2",
+    "url": "/my_react_app/static/js/main.c1b57c02.chunk.js"
+  },
+  {
+    "revision": "bdb727d7188bbea73cb5",
+    "url": "/my_react_app/static/js/runtime-main.3ca5d344.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
