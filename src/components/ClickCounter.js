5357599a4918d4bc59b9db68197df42525742777
Ashfaque <ashfaque.b.shaikh@accenture.com>
2020-03-12 16:48:48 +0530

    some changes made

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 class ClickCounter extends React.Component {
 
+	constructor(){
+		super();
+		this.state = {
+			"counter" : 0
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(e) {
+		this.setState((state) => {
+			return {counter: state.counter+1}
+		});
+	}
 	render(){
 		return (
 			<div>
-				<button>Click Me</button>
-				<p>Counter</p>
+				<button onClick={this.handleClick}>Click Me</button>
+				<p>Counter : {this.state.counter}</p>
 			</div>
 		)
 	}
