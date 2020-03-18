911defefbff017243d44aa253792b9353cca3e8d
Ashfaque <ashfaque.b.shaikh@accenture.com>
2020-03-18 09:41:12 +0530

    Updates

--- precache-manifest.8041c67c66f64a20bdaaab7188eeec19.js
+++ precache-manifest.8041c67c66f64a20bdaaab7188eeec19.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1e870a77ff23863ad885d6b90d6edb3",
+    "revision": "91b9a8518cde08a90e09215e500c56b7",
     "url": "/my_react_app/index.html"
   },
   {
-    "revision": "8c5882e60cf7b58a5d72",
-    "url": "/my_react_app/static/css/main.5bc7f2ad.chunk.css"
+    "revision": "97366097c1026e6d39d1",
+    "url": "/my_react_app/static/css/main.fb095467.chunk.css"
   },
   {
     "revision": "8fbd96002222dd3a583d",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my_react_app/static/js/2.b9a14463.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8c5882e60cf7b58a5d72",
-    "url": "/my_react_app/static/js/main.f721ea23.chunk.js"
+    "revision": "97366097c1026e6d39d1",
+    "url": "/my_react_app/static/js/main.7115cd76.chunk.js"
   },
   {
     "revision": "bdb727d7188bbea73cb5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_react_app/precache-manifest.fc5ef4071c94589a9382e31ba83cc52d.js"
+  "/my_react_app/precache-manifest.8041c67c66f64a20bdaaab7188eeec19.js"
 );
 
 self.addEventListener('message', (event) => {
